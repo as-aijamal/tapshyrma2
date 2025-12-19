bf5f73f342213275d917de4365d03a3aa8665c7e
as-aijamal <a.asangazieva@gmail.com>
2025-12-19 10:59:03 +0600

    dagy ozgorttum

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Ozgoruu kirgizdik");
+        System.out.println("Dage ozgortuu kirgizdim");
     }
 }
